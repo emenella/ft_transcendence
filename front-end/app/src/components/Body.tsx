a2a23c4039392c6824dd5691aa8e2c250c7aac91
Manon Berne <manon.berne97@gmail.com>
2023-02-14 15:26:37 +0100

    better structure, add user status

---
+++ front-end/app/src/components/Body.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NotConnected from './User_not_connected';
+import Connected from './User_connected';
+import Game from './Game';
+
+interface User {
+    status: boolean;
+    playing: boolean;
+}
+
+function Body({status, playing} : User) {
+    if (status && playing)
+        return <Game />;
+    else if (status)
+        return <Connected />;
+    else
+        return <NotConnected />;
+}
+
+export default Body;
--- front-end/app/src/components/Header.tsx
+++ front-end/app/src/components/Header.tsx
@@ -2,19 +2,37 @@ import React from 'react';
 import './Header.css';
 import logo from '../assets/black_logo.png';
 
-function Header() {
-  return (
-    <div className='flex-container'>
+interface User {
+  status: boolean;
+}
+
+function Header({status}: User) {
+  if (status)
+    return (
+      <div className='flex-container'>
       <div> <img src={logo} alt='Logo du site' /> </div>
       <div> <h1>Le meilleur jeu de pong de tout 42</h1> </div>
       <div>
         <p>
-          Connexion<br /> {/* ou Paramètres de compte */}
-          Création d'un compte {/* ou Déconnexion */}
+          Paramètres de compte<br />
+          Déconnexion
         </p>
       </div>
     </div>
-  );
+    );
+  else
+    return (
+      <div className='flex-container'>
+      <div> <img src={logo} alt='Logo du site' /> </div>
+      <div> <h1>Le meilleur jeu de pong de tout 42</h1> </div>
+      <div>
+        <p>
+          Connexion<br />
+          Création d'un compte
+        </p>
+      </div>
+    </div>
+    );
 }
 
 export default Header;
