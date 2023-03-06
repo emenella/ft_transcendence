aaea26b81bad17eef4f44aafebe5bdefe7bc6613
Manon Berne <manon.berne97@gmail.com>
2023-03-06 20:24:24 +0100

    remove api call for logout

--- front-end/src/components/Header_connected.tsx
+++ front-end/src/components/Header_connected.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { tokenFunction } from '../utils/interface';
 import { Link } from "react-router-dom";
-import { logOutApi } from '../api/Logout';
 
 class HeaderConnected extends React.Component<tokenFunction> {
-    logout() {
-        logOutApi();
-        localStorage.removeItem("user");
-    }
-    
     render() {
         return (
             <div>
                 <Link to="/accountmanagement">Paramètres de compte</Link>
                 <br />
-                <button onClick= {() => { this.logout(); this.props.setToken(); }}>Déconnexion</button>
+                <button onClick= {() => { localStorage.removeItem("user"); this.props.setToken(); }}>Déconnexion</button>
             </div>
         );
     }
--- front-end/src/components/button/LoginButton.tsx
+++ front-end/src/components/button/LoginButton.tsx
@@ -3,15 +3,6 @@ import { tokenFunction } from "../../utils/interface";
 import { connexion } from "../../api/Login";
 
 class LoginButton extends React.Component<tokenFunction> {
-// 	const [url, setUrl] = React.useState<string>();
-// 	React.useEffect(() => {
-// 		const getUrl = async () => {
-// 			const urltmp = await connexion();
-// 			setUrl(urltmp);
-// 		};
-// 		getUrl();
-// 	}, []);
-
 	render() {
 		return (
 			// <a href={url}>
