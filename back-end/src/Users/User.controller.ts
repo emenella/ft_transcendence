c809727fe1f6ba371924b67b54e7ef843c6729fe
emenella <emenella@student.42lyon.fr>
2023-03-10 18:08:08 +0100

    indentation

--- back-end/src/Users/User.controller.ts
+++ back-end/src/Users/User.controller.ts
@@ -8,75 +8,75 @@ import { UserService } from "./service/User.service";
 @Controller("users")
 export class UserControllers {
     constructor(private readonly userService: UserService) {}
-
+    
     @Get("/me/")
     async getMe(@Req() req : Request): Promise<User> {
         return req.user as User;
     }
-
+    
     @Get("/")
     async getUser(): Promise<User[]> {
         return this.userService.getAllUsers();
     }
-
+    
     @Get("/id/")
     async getUserById(@Query('id') id: number): Promise<User> {
         return this.userService.getUserById(id);
     }
-
+    
     @Get("/username/")
     async getUserByLogin(@Query('username') username: string): Promise<User> {
         return this.userService.getUserByLogin(username);
     }
-
+    
     @Post("/me/")
     async updateUser(@Req() req : Request, @Body("username") username: string): Promise<User> {
         const user: User = await this.getMe(req);
         console.log(username);
         return this.userService.updateUsername(user.id, username);
     }
-
-	@Get("/friends/")
+    
+    @Get("/friends/")
     async getFriends(@Req() req: Request): Promise<User[]> {
         const user: User = await this.getMe(req);
         return this.userService.getFriends(user);
     }
-
-	@Post("/friends/add")
+    
+    @Post("/friends/add")
     async addFriend(@Req() req: Request, @Query('friendId') friendId: number): Promise<void> {
         const user: User = await this.getMe(req);
         console.log("addFriend " + friendId + " to " + user.id);
-		const friend: User = await this.getUserById(friendId);
+        const friend: User = await this.getUserById(friendId);
         await this.userService.addFriend(user, friend);
     }
-
-	@Delete("/friends/remove")
+    
+    @Delete("/friends/remove")
     async removeFriend(@Req() req: Request, @Query('friendId') friendId: number): Promise<void> {
         const user: User = await this.getMe(req);
-		const friend: User = await this.getUserById(friendId);
+        const friend: User = await this.getUserById(friendId);
         await this.userService.removeFriend(user, friend);
     }
-
-	@Get("/blacklist/")
+    
+    @Get("/blacklist/")
     async getBlacklist(@Req() req: Request): Promise<User[]> {
         const user: User = await this.getMe(req);
         return this.userService.getBlacklist(user);
     }
-
-	@Post("/blacklist/add")
+    
+    @Post("/blacklist/add")
     async addBlacklist(@Req() req: Request, @Query('userToBlacklistId') userToBlacklistId: number): Promise<void> {
         const user: User = await this.getMe(req);
-		const userToBlacklist: User = await this.getUserById(userToBlacklistId);
+        const userToBlacklist: User = await this.getUserById(userToBlacklistId);
         await this.userService.addBlacklist(user, userToBlacklist);
     }
-
-	@Delete("/blacklist/remove")
+    
+    @Delete("/blacklist/remove")
     async removeBlacklist(@Req() req: Request, @Query('userToBlacklistId') userToBlacklistId: number): Promise<void> {
         const user: User = await this.getMe(req);
-		const userToBlacklist: User = await this.getUserById(userToBlacklistId);
+        const userToBlacklist: User = await this.getUserById(userToBlacklistId);
         await this.userService.removeBlacklist(user, userToBlacklist);
     }
-
+    
     @Post("/upload/avatar/")
     @UseInterceptors(FileInterceptor('file'))
     async uploadAvatar(@UploadedFile() file: Express.Multer.File, @Req() req: any): Promise<string> {
--- back-end/src/Users/service/User.service.ts
+++ back-end/src/Users/service/User.service.ts
@@ -9,142 +9,142 @@ export class UserService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>
-    ) {}
-
-    async getAllUsers(): Promise<User[]> {
-        const users = await this.userRepository.find({ relations: ["avatar", "winMatch", "looseMatch", "friends"] });
-        return users;
-    }
-
-    async getUserById(id: number): Promise<User> {
-        const user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
-        if (!user)
+        ) {}
+        
+        async getAllUsers(): Promise<User[]> {
+            const users = await this.userRepository.find({ relations: ["avatar", "winMatch", "looseMatch", "friends"] });
+            return users;
+        }
+        
+        async getUserById(id: number): Promise<User> {
+            const user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
+            if (!user)
             throw new HttpException(`User with ID ${id} not found.`, 404);
-        return user;
-    }
-
-    async getUserByLogin(username: string): Promise<User> {
-        const user = await this.userRepository.findOne({ where: { username: username }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
-        if (!user)
+            return user;
+        }
+        
+        async getUserByLogin(username: string): Promise<User> {
+            const user = await this.userRepository.findOne({ where: { username: username }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
+            if (!user)
             throw new HttpException(`User with username ${username} not found.`, 404);
-        return user;
-    }
-
-    async createUser(body: User): Promise<User> {
-        return await this.userRepository.save(body);
-    }
-
-    async updateUsername(id: number, username: string): Promise<User> {
-        const userToUpdate = await this.userRepository.findOne({where : { id: id }});
-        if(!username)
+            return user;
+        }
+        
+        async createUser(body: User): Promise<User> {
+            return await this.userRepository.save(body);
+        }
+        
+        async updateUsername(id: number, username: string): Promise<User> {
+            const userToUpdate = await this.userRepository.findOne({where : { id: id }});
+            if(!username)
             throw new HttpException(`Username is empty.`, 400);
-        if (!userToUpdate) {
-            throw new HttpException(`User with ID ${id} not found.`, 404);
+            if (!userToUpdate) {
+                throw new HttpException(`User with ID ${id} not found.`, 404);
+            }
+            userToUpdate.username = username;
+            userToUpdate.isProfileComplete = true;
+            return await this.userRepository.save(userToUpdate);
         }
-        userToUpdate.username = username;
-        userToUpdate.isProfileComplete = true;
-        return await this.userRepository.save(userToUpdate);
-    }
-
-    async updateUser(id: number, updatedUser: User): Promise<User> {
-        const userToUpdate = await this.userRepository.findOne({where : { id: id }});
-        if (!userToUpdate) {
-            throw new HttpException(`User with ID ${id} not found.`, 404);
-        }
-        if (updatedUser.username)
+        
+        async updateUser(id: number, updatedUser: User): Promise<User> {
+            const userToUpdate = await this.userRepository.findOne({where : { id: id }});
+            if (!userToUpdate) {
+                throw new HttpException(`User with ID ${id} not found.`, 404);
+            }
+            if (updatedUser.username)
             userToUpdate.username = updatedUser.username;
-        if (updatedUser.connection)
+            if (updatedUser.connection)
             userToUpdate.connection = updatedUser.connection;
-        return await this.userRepository.save(userToUpdate);
-    }
-
-    async deleteUser(id: number): Promise<void> {
-        await this.userRepository.delete(id);
-    }
-
-    async getUserFromConnectionId(connectionId: number): Promise<User> {
-        const user = await this.userRepository.findOne({ where: { connection: {id: connectionId} }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
-        if (!user)
+            return await this.userRepository.save(userToUpdate);
+        }
+        
+        async deleteUser(id: number): Promise<void> {
+            await this.userRepository.delete(id);
+        }
+        
+        async getUserFromConnectionId(connectionId: number): Promise<User> {
+            const user = await this.userRepository.findOne({ where: { connection: {id: connectionId} }, relations: ["avatar", "winMatch", "looseMatch", "friends"] });
+            if (!user)
             throw new HttpException(`User with connectionID ${connectionId} not found.`, 404);
-        return user;
-    }
-
-    async uploadAvatar(id: number, file: Express.Multer.File): Promise<string> {
-        let user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar"] });
-        if (!user)
+            return user;
+        }
+        
+        async uploadAvatar(id: number, file: Express.Multer.File): Promise<string> {
+            let user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar"] });
+            if (!user)
             throw new HttpException(`User with ID ${id} not found.`, 404);
-        if (!user.isProfileComplete)
+            if (!user.isProfileComplete)
             throw new HttpException(`User with ID ${id} has not completed his profile.`, 400);
-        if (!user.avatar)
-        {
-            user.avatar = new Avatar();
-            user.avatar.user = user;
-            user.avatar.path = file.path;
-        }
-        else
-        {
-            user.avatar.path = file.path;
-        }
-        await this.userRepository.save(user);
-        return file.path;
-    }
-
-	async getFriends(user: User): Promise<User[]> {
-		return user.friends;
-	}
-
-	async addFriend(user: User, friend: User): Promise<void> {
-        if (user.friends.some(f => f.id === friend.id)) {
-            throw new HttpException(`User with ID ${friend.id} is already a friend of User with ID ${user.id}.`, 400);
-        } else {
-            user.friends.push(friend);
-            user.friends.sort((a, b) => a.id - b.id);
+            if (!user.avatar)
+            {
+                user.avatar = new Avatar();
+                user.avatar.user = user;
+                user.avatar.path = file.path;
+            }
+            else
+            {
+                user.avatar.path = file.path;
+            }
             await this.userRepository.save(user);
+            return file.path;
         }
-    }
-    
-
-	async removeFriend(user: User, friend: User): Promise<void> {
-		const friendIndex = user.friends.findIndex(f => f.id === friend.id);
-        if (friendIndex === -1) {
-            throw new HttpException(`User with ID ${friend.id} is not a friend of User with ID ${user.id}.`, 400);
+        
+        async getFriends(user: User): Promise<User[]> {
+            return user.friends;
         }
-        else {
-            user.friends.splice(friendIndex, 1);
-            await this.userRepository.save(user);
+        
+        async addFriend(user: User, friend: User): Promise<void> {
+            if (user.friends.some(f => f.id === friend.id)) {
+                throw new HttpException(`User with ID ${friend.id} is already a friend of User with ID ${user.id}.`, 400);
+            } else {
+                user.friends.push(friend);
+                user.friends.sort((a, b) => a.id - b.id);
+                await this.userRepository.save(user);
+            }
         }
-    }
-
-	async getBlacklist(user: User): Promise<User[]> {
-		return user.blacklist;
-	}
-
-	async addBlacklist(user: User, userToBlacklist: User): Promise<void> {
-		if (user.blacklist.some(f => f.id === userToBlacklist.id))
-			throw new HttpException(`User with ID ${userToBlacklist.id} is already blocked by User with ID ${user.id}.`, 400);
-		else {
-			user.blacklist.push(userToBlacklist);
-			user.blacklist.sort();
-            await this.userRepository.save(user);
-		}
-	}
-
-	async removeBlacklist(user: User, userToBlacklist: User): Promise<void> {
-		if (!user.blacklist.some(f => f.id === userToBlacklist.id))
-			throw new HttpException(`User with ID ${userToBlacklist.id} is not blocked by User with ID ${user.id}.`, 400);
-		else {
-			user.blacklist.splice(user.friends.indexOf(userToBlacklist), 1);
-            await this.userRepository.save(user);
+        
+        
+        async removeFriend(user: User, friend: User): Promise<void> {
+            const friendIndex = user.friends.findIndex(f => f.id === friend.id);
+            if (friendIndex === -1) {
+                throw new HttpException(`User with ID ${friend.id} is not a friend of User with ID ${user.id}.`, 400);
+            }
+            else {
+                user.friends.splice(friendIndex, 1);
+                await this.userRepository.save(user);
+            }
         }
-	}
-
-    async getAvatar(id: number): Promise<Avatar> {
-        const user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar"] });
-        if (!user)
+        
+        async getBlacklist(user: User): Promise<User[]> {
+            return user.blacklist;
+        }
+        
+        async addBlacklist(user: User, userToBlacklist: User): Promise<void> {
+            if (user.blacklist.some(f => f.id === userToBlacklist.id))
+            throw new HttpException(`User with ID ${userToBlacklist.id} is already blocked by User with ID ${user.id}.`, 400);
+            else {
+                user.blacklist.push(userToBlacklist);
+                user.blacklist.sort();
+                await this.userRepository.save(user);
+            }
+        }
+        
+        async removeBlacklist(user: User, userToBlacklist: User): Promise<void> {
+            if (!user.blacklist.some(f => f.id === userToBlacklist.id))
+            throw new HttpException(`User with ID ${userToBlacklist.id} is not blocked by User with ID ${user.id}.`, 400);
+            else {
+                user.blacklist.splice(user.friends.indexOf(userToBlacklist), 1);
+                await this.userRepository.save(user);
+            }
+        }
+        
+        async getAvatar(id: number): Promise<Avatar> {
+            const user = await this.userRepository.findOne({ where: { id: id }, relations: ["avatar"] });
+            if (!user)
             throw new HttpException(`User with ID ${id} not found.`, 404);
-        if (!user.avatar)
+            if (!user.avatar)
             throw new HttpException(`User with ID ${id} has no avatar.`, 404);
-        return user.avatar;
+            return user.avatar;
+        }
+        
     }
-
-}
