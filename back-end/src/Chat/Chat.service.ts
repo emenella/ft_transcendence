855713d73da856813a27cdc296720c438aa93ab2
Emil-Bellon <emil.bellon@gmail.com>
2023-03-15 21:31:30 +0100

    gestion de commande dans les chan fini ?

--- back-end/src/Chat/Chat.service.ts
+++ back-end/src/Chat/Chat.service.ts
@@ -158,9 +158,34 @@ export class ChatService {
                     return true;
                 }
                 server.to(ret.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: bannedUser.username + ' has been banned !'});
-                socket.leave(ret.id.toString());
-                socket.emit('error', 'You have been ban from ' + ret.title + ' channel.\nPay attention to your behavior next time !');
-                socket.emit('ban', ret.title);
+                bannedUser.socket.leave(ret.id.toString());
+                bannedUser.socket.emit('error', 'You have been ban from ' + ret.title + ' channel for ' + duration + ' min.\nPay attention to your behavior next time !');
+                bannedUser.socket.emit('ban', ret.title);
+                return true;
+            }
+            case "/unban" : {
+                if (isAdm === false) {
+                    socket.emit('error', 'Only admin or owner can send command !');
+                    return true;
+                }
+                if (command.length != 2) {
+                    socket.emit('error', 'ban command need exactly one argument : /unban <Username>.');
+                    return true;
+                }
+
+                const unbannedUser : ChatUser | undefined = this.getUserFromUsername(command[1]);
+                if (unbannedUser === undefined) {
+                    socket.emit('error', 'User: ' + command[1] + ' does not exist !');
+                    return true;
+                }
+
+                const ret : Chan | string = await this.chanService.unbanUser(chan.id, user.id, unbannedUser.id);
+                if (typeof ret === 'string') {
+                    socket.emit('error', ret);
+                    return true;
+                }
+                server.to(ret.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: unbannedUser.username + ' has been unbanned !'});
+                unbannedUser.socket.emit('error', 'You have been unban from ' + ret.title + ' channel.\nYou can join it again in join section !');
                 return true;
             }
             case "/mute" : {
@@ -180,7 +205,7 @@ export class ChatService {
                 }
                 const duration : number = parseInt(command[2]);
                 if (duration <= 0) {
-                    socket.emit('error', 'ban duration is 1 minute minimum');
+                    socket.emit('error', 'mute duration is 1 minute minimum');
                     return true;
                 }
 
@@ -194,16 +219,108 @@ export class ChatService {
                     return true;
                 }
                 server.to(chan.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: mutedUser.username + ' has been muted !'});
-                socket.emit('error', 'You have been mute in ' + chan.title + ' channel.\nBe carefull not to be banned !');
+                socket.emit('error', 'You have been mute in ' + chan.title + ' channel for ' + duration + ' min.\nBe carefull not to be banned !');
+                return true;
+            }
+            case "/unmute" : {
+                if (isAdm === false) {
+                    socket.emit('error', 'Only admin or owner can send command !');
+                    return true;
+                }
+                if (command.length != 2) {
+                    socket.emit('error', 'unmute command need exactly one argument : /mute <Username>.');
+                    return true;
+                }
+
+                const mutedUser : ChatUser | undefined = this.getUserFromUsername(command[1]);
+                if (mutedUser === undefined) {
+                    socket.emit('error', 'User: ' + command[1] + ' does not exist !');
+                    return true;
+                }
+
+                const ret : boolean | string = await this.chanService.chanUnmute(chan.id, user.id, mutedUser.id);
+                if (typeof ret === 'string') {
+                    socket.emit('error', ret);
+                    return true;
+                }
+                server.to(chan.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: mutedUser.username + ' has been unmuted !'});
+                socket.emit('error', 'You have been unmute in ' + chan.title + ' channel !');
                 return true;
             }
             case "/promote" : {
+                if (user.id !== chan.owner.id) {
+                    socket.emit('error', 'Only owner can send promote command !');
+                    return true;
+                }
+                if (command.length != 2) {
+                    socket.emit('error', 'promote command need exactly one argument : /promote <Username>.');
+                    return true;
+                }
+
+                const promotedUser : ChatUser | undefined = this.getUserFromUsername(command[1]);
+                if (promotedUser === undefined) {
+                    socket.emit('error', 'User: ' + command[1] + ' does not exist !');
+                    return true;
+                }
+                const ret : undefined | string = await this.chanService.setIsAdmin(chan.id, user.id, promotedUser.id, true);
+                if (typeof ret === 'string') {
+                    socket.emit('error', ret);
+                    return true;
+                }
+                server.to(chan.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: promotedUser.username + ' is now an Admin !'})
+                promotedUser.socket.emit('error', 'You have been promoted to admin in ' + chan.title + ' channel !\nRemember : With great power comes great responsabilities.');
                 return true;
             }
             case "/demote" : {
+                if (user.id !== chan.owner.id) {
+                    socket.emit('error', 'Only owner can send demote command !');
+                    return true;
+                }
+                if (command.length != 2) {
+                    socket.emit('error', 'demote command need exactly one argument : /demote <Username>.');
+                    return true;
+                }
+
+                const demotedUser : ChatUser | undefined = this.getUserFromUsername(command[1]);
+                if (demotedUser === undefined) {
+                    socket.emit('error', 'User: ' + command[1] + ' does not exist !');
+                    return true;
+                }
+                const ret : undefined | string = await this.chanService.setIsAdmin(chan.id, user.id, demotedUser.id, false);
+                if (typeof ret === 'string') {
+                    socket.emit('error', ret);
+                    return true;
+                }
+                server.to(chan.id.toString()).emit('msgToClient', {author: user.username, chan: chan.title, msg: demotedUser.username + ' is no longer an Admin !'})
+                demotedUser.socket.emit('error', 'You have been demoted to admin in ' + chan.title + ' channel !\nRemember : With great power comes great responsabilities.');
                 return true;
             }
             case "/password" : {
+                if (user.id !== chan.owner.id) {
+                    socket.emit('error', 'Only owner can change the password !');
+                    return true;
+                }
+                if (command.length > 2) {
+                    socket.emit('error', 'password command need exactly one or no argument : /password <newPassword>.\n(leave empty to disable password');
+                    return true;
+                }
+
+                if (command.length === 2) {
+                    const ret : string | undefined = await this.chanService.chanChangePass(chan.id, user.id, command[1]);
+                    if (typeof ret === 'string') {
+                        socket.emit('error', ret);
+                        return true;
+                    }
+                    socket.emit('error', 'Password changed to ' + command[1] + '.');
+                }
+                else {
+                    const ret : string | undefined = await this.chanService.chanChangePass(chan.id, user.id, null);
+                    if (typeof ret === 'string') {
+                        socket.emit('error', ret);
+                        return true;
+                    }
+                    socket.emit('error', 'Password disable.');
+                }
                 return true;
             }
             default : {
