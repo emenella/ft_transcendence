3f7a4031ae8104f9fb460b38558e4694fddf8dba
emenella <emenella@student-42lyon.fr>
2023-04-17 17:21:36 +0200

    fic Profil

--- front-end/src/components/Profil.tsx
+++ front-end/src/components/Profil.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import './Profil.css';
 import { getMe, getMatchs, getUserById } from '../api/User';
 import Emoji from './Emoji';
